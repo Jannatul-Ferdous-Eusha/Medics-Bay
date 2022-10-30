6179d93ed97772c9df695ddafb31eae68d634965
Sadia Sathi <sadia.nasrin10@northsouth.edu>
2022-10-30 23:02:16 +0600

    Navbar, Footer, Home

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const mongoose = require("mongoose")
 
 const routes = require("./routes/main");
 
+// Path setting for Image, css and js
 app.use('/static', express.static("public"))
+
 app.use('', routes)
 
 //template engine (hbs)
@@ -20,6 +22,6 @@ mongoose.connect("mongodb://localhost/doctors-portal", () => {
     console.log("db connected")
 })
 
-app.listen(process.env.PORT | 5556, () => {
+app.listen(process.env.PORT | 3000, () => {
     console.log("Server Start");
 });
