ea4f97ed6a1bd915989fa7c914992232b87b28b2
Sadia Sathi <sadia.nasrin10@northsouth.edu>
2022-11-20 22:26:23 +0600

    db connection delete from app.js

--- src/app.js
+++ src/app.js
@@ -4,28 +4,28 @@ const hbs = require("hbs")
 const app = express();
 const mongoose = require("mongoose");
 require("./db/conn");
-const Register=require("./models/register");
-const cookieParser=require("cookie-parser");
+const Register = require("./models/register");
+const cookieParser = require("cookie-parser");
 
 const routes = require("./routes/main");
 
 //get user details in json(2), for cookies(3rd)
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Path setting for Image, css and js
 app.use('/static', express.static("public"));
 
 // dynamic header login logout
-app.use(function(req,res,next){ 
-    const token=req.cookies.jwt;
-    if(token==null){
-        res.locals.isAuthenticated=false;
-    }else{
-        res.locals.isAuthenticated=true;
+app.use(function (req, res, next) {
+    const token = req.cookies.jwt;
+    if (token == null) {
+        res.locals.isAuthenticated = false;
+    } else {
+        res.locals.isAuthenticated = true;
     }
-    
+
     next();
 })
 
@@ -37,9 +37,9 @@ app.set("views", "views")
 hbs.registerPartials("views/partials")
 
 //user registration
-app.post("/register", async(req,res) =>{
+app.post("/register", async (req, res) => {
     try {
-        const registerUser=new Register({
+        const registerUser = new Register({
             fullname: req.body.fname,
             email: req.body.email,
             password: req.body.pass,
@@ -48,28 +48,28 @@ app.post("/register", async(req,res) =>{
             gender: req.body.gender,
             birthday: req.body.birthday
         })
-        const email=req.body.email;
+        const email = req.body.email;
         //check if user exists
-        const userExists=await Register.findOne({email:email});
-        if(userExists!=null){
-            res.render("signin",{
+        const userExists = await Register.findOne({ email: email });
+        if (userExists != null) {
+            res.render("signin", {
                 msg: "Email address is already registered"
             });
-        }else{
+        } else {
             console.log("user not exists");
-        //generate jwt
-        const token=await registerUser.generateAuthToken();
+            //generate jwt
+            const token = await registerUser.generateAuthToken();
 
-        //create cookies & save jwt
-        res.cookie("jwt",token,{
-            expires:new Date(Date.now()+300000), //expires in 5 mins
-            httpOnly: true    //client side can not delete cookie
-        });
+            //create cookies & save jwt
+            res.cookie("jwt", token, {
+                expires: new Date(Date.now() + 300000), //expires in 5 mins
+                httpOnly: true    //client side can not delete cookie
+            });
 
-        //data "get" done now save it
-        const registered = await registerUser.save();
-        console.log("User registered successfully");
-        res.status(201).redirect("/");
+            //data "get" done now save it
+            const registered = await registerUser.save();
+            console.log("User registered successfully");
+            res.status(201).redirect("/");
         }
     } catch (error) {
         res.status(400).send(error);
@@ -78,26 +78,26 @@ app.post("/register", async(req,res) =>{
 })
 
 //user login
-app.post("/login", async(req,res) =>{
+app.post("/login", async (req, res) => {
     try {
-        const email=req.body.email;
-        const pass=req.body.pass;
-        const userLoggedInCreds=await Register.findOne({email:email}); //first email is of the db field second is of the user input
-        if(userLoggedInCreds==null){
-            res.render("login",{
+        const email = req.body.email;
+        const pass = req.body.pass;
+        const userLoggedInCreds = await Register.findOne({ email: email }); //first email is of the db field second is of the user input
+        if (userLoggedInCreds == null) {
+            res.render("login", {
                 msg: "Invalid email or password"
             });
-        }else{
-            if(userLoggedInCreds.password===pass){
+        } else {
+            if (userLoggedInCreds.password === pass) {
                 console.log("User logged in successfully");
-                const token=await userLoggedInCreds.generateAuthToken();
-                res.cookie("jwt",token,{
-                    expires:new Date(Date.now()+300000), //expires in 5 mins
+                const token = await userLoggedInCreds.generateAuthToken();
+                res.cookie("jwt", token, {
+                    expires: new Date(Date.now() + 300000), //expires in 5 mins
                     httpOnly: true,    //client side can not delete cookie
                 });
                 res.status(201).redirect("user-profile");
-            }else{
-                res.render("login",{
+            } else {
+                res.render("login", {
                     msg: "Invalid email or password"
                 });
             }
@@ -106,12 +106,6 @@ app.post("/login", async(req,res) =>{
         res.status(400).send(error)
     }
 })
-
-//db connection
-
-// mongoose.connect("mongodb://localhost/doctors-portal", () => {
-//     console.log("db connected")
-// })
 
 app.listen(process.env.PORT | 3000, () => {
     console.log("Server Start");
